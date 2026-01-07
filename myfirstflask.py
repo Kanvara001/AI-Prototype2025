ff3711f340c2a231e3c84c4a68a19e75c3722198
Kanvara001 <kanvara.t@kkumail.com>
2026-01-07 09:05:48 +0700

    Create myfirstflask.py

---
+++ myfirstflask.py
@@ -0,0 +1,42 @@
+from flask import Flask,  flash, redirect, request, render_template, make_response, url_for
+import json
+import sys 
+#import pandas as pd
+
+app = Flask(_name_)
+
+
+@app.route("/")  #บอกว่าเรียกใช้ web ไหน
+def helloworld():
+    return "Hello, World!"
+
+@app.route("/name")
+def name():
+    return "Kanvara"
+
+@app.route('/receive_get',methods=['GET']) 
+def web_service_API_GET():
+
+    msg = request.args.get('msg')
+    name = request.args.get('name')
+
+    print(f'the input message from GET is {msg} from {name}.')
+
+    return f'{msg} from {name} receive!'
+
+@app.route('/request',methods=['POST']) 
+def web_service_API_POST():
+
+    payload = request.data.decode("utf-8")
+    inmessage = json.loads(payload) # ทำ json
+
+    print(inmessage)
+    
+    
+    json_data = json.dumps({'y': 'received!'}) # ส่งกลับไปว่าได้รับเเล้ววว
+    return json_data
+
+
+
+if _name_ == "_main_":   # run code 
+    app.run(host='localhost',debug=True,port=5001)#host='0.0.0.0' = run on internet ,port=5001
