be5969f43fe45e27fd4242aa684c859e12d2b145
Kanvara <kanvara.t@kkumail.com>
2026-01-07 09:49:43 +0700

    myfirstflask

--- myfirstflask.py
+++ myfirstflask.py
@@ -5,14 +5,17 @@ import sys
 
 app = Flask(__name__)
 
-@app.route("/")  # 
+#web service
+##api post
+@app.route("/")  #บอกว่าเรียกใช้ web ไหน
 def helloworld():
     return "Hello, World!"
 
+@app.route("/name")  #บอกว่าเรียกใช้ web ไหน
+def name():
+    return "Kanvara"
 
 
-#web service
-##api post
 @app.route('/request',methods=['POST']) 
 def web_service_API():
 
@@ -21,55 +24,8 @@ def web_service_API():
 
     print(inmessage)
     
-    
     json_data = json.dumps({'y': 'received!'}) # ส่งกลับไปว่าได้รับเเล้ววว
     return json_data
 
-
-
-@app.route("/")  #บอกว่าเรียกใช้ web ไหน
-def helloworld():
-    return "Hello, World!"
-
-@app.route("/name")  #บอกว่าเรียกใช้ web ไหน
-def hellonat():
-    return "Hello, Nat!"
-
-@app.route("/home", methods=['POST','GET']) # methods=['POST'] ต้องเปิดเพื่อให้รับข้อความ
-def homefn():
-    if request.method == "GET":
-       print('we are in home(GET)', file=sys.stdout)
-
-       namein = request.args.get('fname')
-       print(namein, file=sys.stdout)
-       return render_template("home.html",name=namein)
-
-    elif request.method == "POST":
-        print('we are in home(POST)', file=sys.stdout)
-        namein = request.form.get('fname') #เก็บ input
-        lastnamein = request.form.get('lname')
-        print(namein, file=sys.stdout)
-        print(lastnamein, file=sys.stdout)
-        #return render_template("home.html",name = f"{first_name} {last_name}")
-        return render_template("home.html",name=namein)
-
-@app.route('/upload', methods=['GET', 'POST'])  # เริ่มเเรก Get ไป return
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        file.save('filename')
-        return render_template("home.html",name= 'upload completed')
-
-    return '''  
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-
-
-if __name__ == "__main__":   # run code 
+if _name_ == "_main_":   # run code 
     app.run(host='0.0.0.0',debug=True,port=5001)#host='0.0.0.0' = run on internet ,port=5001
